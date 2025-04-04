a7e27e9706c93c17a94846401fbd84c144e4ada0
Liliana321 <lilianagilca0@gmail.com>
2025-04-04 07:16:06 +0200

    Create primi_cicli.py

---
+++ Lunedi 31marzo/primi_cicli.py
@@ -0,0 +1,47 @@
+
+# conteggio matematico - ciclo matematico 
+
+""" conteggio =0
+
+while conteggio<5:
+    print(conteggio)
+    conteggio +=1 """
+    
+#conteggio booleano - ciclo booleano 
+
+""" controller = True
+
+while controller:
+    scelta = input("vuoi continuare? ")
+    if scelta.lower() == "no":
+        controller = False
+    else:
+        print("stai continuando") 
+         
+ """
+
+#ciclo for: prende ogni elemento della lista e stampa
+
+""" lista =[1,2,3,4,5]
+
+for i in lista:
+    print(i)
+    
+parola="Liliana"
+
+for i in parola:
+    print(i)
+     """
+
+#stampa da 0 a 4
+   
+for i in range(5):
+    print (i)
+
+#stampa da 2 a 7 
+for i in range(2,8):
+    print (i)
+    
+#stampa da 1 a 9 solo numeri dispari 
+for i in range(1,10,2):
+    print (i)
