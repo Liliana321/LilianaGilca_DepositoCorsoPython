d590e633777ec0d30f734f8d27144919edb2e54e
Liliana321 <lilianagilca0@gmail.com>
2025-04-18 09:23:50 +0200

    Create esempio_modulo.py

---
+++ Lunedi 14 Aprile/esempio_modulo.py
@@ -0,0 +1,21 @@
+import funzioni_varie as mm
+
+print(mm.x)
+
+copia_di_x = mm.x
+nuova_variabile = mm.somma(5, 5)
+
+somma = mm.somma(5, 6)
+print(somma)
+
+
+# dobbiamo importare una classe
+
+
+
+mm.saluta("Alice")  # Stampa "Ciao, Alice"
+
+raggio = 2
+cerchio = mm.Cerchio(raggio)
+print(cerchio.area())       # Stampa l'area del cerchio con raggio 2
+
