11269444fd93be8caa71230bb54b159e2cef745f
Liliana321 <lilianagilca0@gmail.com>
2025-04-24 10:15:55 +0200

    Update lezione17.py

--- Martedi 15 Aprile/lezione17.py
+++ Martedi 15 Aprile/lezione17.py
@@ -4,6 +4,8 @@ import numpy as np
 # Creazione di un array
 array = np.array([10, 20, 30, 40, 50])
 
+
+
 # Utilizzo di un array di indici
 indici = np.array([1, 3])
 print(array[indici])  # Output: [20 40]
@@ -11,3 +13,15 @@ print(array[indici])  # Output: [20 40]
 # Utilizzo di una lista di indici
 indici = [0, 2, 4]
 print(array[indici])  # Output: [10 30 50]
+
+'''
+ARRAY
+
+Operazioni matematiche vettoriali (somma, sottrazione, moltiplicazione, ecc.) in modo veloce e comodo
+
+Indicizzazione avanzata (come la fancy indexing che hai visto prima)
+
+Gestione di array multidimensionali (matrici, tensor, ecc.)
+
+
+'''
